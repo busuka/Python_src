4cf211ae816cf3c21f2ccd414a54efb64aa089c1
Soncho <ara.sho0331@gmail.com>
2015-08-23 03:03:26 +0900

    Python_windows

---
+++ src/Practice/skilearn_test.py
@@ -0,0 +1,8 @@
+__author__ = 'Owner'
+from sklearn.datasets import load_digits
+digits = load_digits()
+print(digits.data.shape)
+import pylab as pl
+pl.gray()
+pl.matshow(digits.images[0])
+pl.show()
---
+++ src/Practice/test_matplotlib.py
@@ -0,0 +1,12 @@
+__author__ = 'Owner'
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(plt.get_backend())
+x = np.arange(-3, 3, 0.1)
+y = np.sin(x)
+plt.plot([1,2,3])
+plt.plot(x, y)
+plt.show()

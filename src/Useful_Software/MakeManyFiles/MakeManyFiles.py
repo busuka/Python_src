11cd495fcc3dae7592a277a8800508f33c422340
busuka <ara.sho0331@gmail.com>
2015-09-13 05:11:35 +0900

    no message

--- src/Useful_Software/MakeManyFiles/MakeManyFiles.py
+++ src/Useful_Software/MakeManyFiles/MakeManyFiles.py
@@ -1,5 +1,10 @@
-import os
+#たくさんのファイルを作成する.
 
+import os
+os.getcwd()                 #カレントフォルダを表示
+os.chdir("./MakeManyFiles") #フォルダを移動して新ファイルを作るフォルダに指定
+
+#UNIXのdateを使い新ファイルを自動で100個作る
 for x in range(1,101):
     f_names = str(x)
     p_names = ".txt"
--- src/Useful_Software/MakeManyFiles/MakeManyForde.py
+++ src/Useful_Software/MakeManyFiles/MakeManyForde.py
@@ -1,11 +1,9 @@
-#たくさんのファイルを作成する.
+#たくさんのフォルダを作成する.
 
 import os
-os.getcwd()                 #カレントフォルダを表示
-os.chdir("./MakeManyFiles") #フォルダを移動して新ファイルを作るフォルダに指定
+print(os.getcwd())                 #カレントフォルダを表示
 
-#UNIXのdateを使い新ファイルを自動で100個作る
+#新フォルダを自動で100個作る
 for x in range(1,101):
     f_names = str(x)
-    p_names = ".txt"
-    os.system("date > " + f_names + p_names)
+    os.mkdir(os.getcwd() + "/" + f_names)
